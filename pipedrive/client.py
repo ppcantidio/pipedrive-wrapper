006bcad9a8142adb544ff65da6cefd2acee36a42
Pedro Cantidio <pedro.cantidio@confitec.com.br>
2023-07-26 11:51:15 -0300

    feat: adding logs to the client service

--- pipedrive/client.py
+++ pipedrive/client.py
@@ -5,10 +5,12 @@ from .deal import Deal
 from .deal_field import DealField
 from .exceptions import BadRequest, Forbidden, InternalServerError, NotFound, TooManyRequests, Unauthorized
 from .person import Person
+import logging
+from logging import Logger
 
 
 class Client:
-    def __init__(self, token: str) -> None:
+    def __init__(self, token: str, logger: Logger = logging) -> None:
         self.base_url = "https://api.pipedrive.com/v1"
         self.token = token
         self.headers = {"Accept": "application/json"}
@@ -16,9 +18,12 @@ class Client:
         self.person = Person(self)
         self.deal = Deal(self)
         self.deal_field = DealField(self)
+        self.logger = logger
 
     def _post(self, url_context: str, body: dict):
         url_to_request = self.__generate_url_to_request(url_context)
+
+        self.extra_log = {'request_url': url_to_request, 'http_method': 'POST'}
 
         body = self.__check_values_of_dict(body)
 
@@ -27,6 +32,12 @@ class Client:
             headers=self.headers,
             json=body,
         )
+
+        extra_log_body_content = self.extra_log.copy()
+        extra_log_body_content['body'] = body
+        self.logger.debug('Body content at extras', extra=extra_log_body_content)
+
+        self.logger.info('Sending request', extra=self.extra_log)
 
         self.__raise_for_status(response)
 
@@ -37,7 +48,15 @@ class Client:
     def _put(self, url_context: str, body: dict):
         url_to_request = self.__generate_url_to_request(url_context)
 
+        self.extra_log = {'request_url': url_to_request, 'http_method': 'PUT'}
+
         body = self.__check_values_of_dict(body)
+
+        self.logger.info('Sending request', extra=self.extra_log)
+
+        extra_log_body_content = self.extra_log.copy()
+        extra_log_body_content['body'] = body
+        self.logger.debug('Body content at extras', extra=extra_log_body_content)
 
         response = requests.put(
             url=url_to_request,
@@ -54,8 +73,14 @@ class Client:
     def _get(self, url_context: str, params: dict = None):
         url_to_request = self.__generate_url_to_request(url_context)
 
+        self.extra_log = {'request_url': url_to_request, 'http_method': 'GET'}
+
         if isinstance(params, dict):
             params = self.__check_values_of_dict(params)
+
+        self.extra_log['query_params'] = params
+
+        self.logger.info('Sending request', extra=self.extra_log)
 
         response = requests.get(
             url=url_to_request,
@@ -81,9 +106,13 @@ class Client:
 
     def __raise_for_status(self, response: requests.Response):
         status_code = response.status_code
+        self.extra_log['http_status_code'] = status_code
 
         if 300 > status_code >= 200:
+            self.logger.info('Successful request', extra=self.extra_log)
             return
+
+        self.logger.error(f'Error when performing request, see extra for details', extra=self.extra_log)
 
         if status_code == 400:
             raise BadRequest()
